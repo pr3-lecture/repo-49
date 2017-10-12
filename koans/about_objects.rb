0b033956ed63c7163e1cb0dc104c7cde65e50d10
murmelmunz <munzinger3@gmail.com>
2017-10-12 22:02:18 +0200

    17

--- koans/about_objects.rb
+++ koans/about_objects.rb
@@ -31,10 +31,10 @@ class AboutObjects < Neo::Koan
   end
 
   def test_small_integers_have_fixed_ids
-    assert_equal Integer, 0.object_id
-    assert_equal Integer, 1.object_id
-    assert_equal Integer, 2.object_id
-    assert_equal Integer, 100.object_id
+    assert_equal 0, 0.object_id
+    assert_equal 1, 1.object_id
+    assert_equal 2, 2.object_id
+    assert_equal 100, 100.object_id
 
     # THINK ABOUT IT:
     # What pattern do the object IDs for small integers follow?
