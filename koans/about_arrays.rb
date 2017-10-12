2c38e307045c1d75156d8e1e0ba65041ad9f2ae9
murmelmunz <munzinger3@gmail.com>
2017-10-12 22:12:10 +0200

    17

--- koans/about_arrays.rb
+++ koans/about_arrays.rb
@@ -24,10 +24,10 @@ class AboutArrays < Neo::Koan
   def test_accessing_array_elements
     array = [:peanut, :butter, :and, :jelly]
 
-    assert_equal __, array[0]
-    assert_equal __, array.first
-    assert_equal __, array[3]
-    assert_equal __, array.last
+    assert_equal :peanut, array[0]
+    assert_equal :peanut, array.first
+    assert_equal :jelly, array[3]
+    assert_equal :jelly, array.last
     assert_equal __, array[-1]
     assert_equal __, array[-3]
   end
