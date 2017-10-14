68917bbdf70f8286f9a96c9d4c664e8bcd0d2303
murmelmunz <munzinger3@gmail.com>
2017-10-14 14:05:26 +0200

    17/282

--- koans/about_arrays.rb
+++ koans/about_arrays.rb
@@ -40,8 +40,8 @@ class AboutArrays < Neo::Koan
     assert_equal [:and, :jelly], array[2,2]
     assert_equal [:and, :jelly], array[2,20]
     assert_equal [], array[4,0]
-    assert_equal __, array[4,100]
-    assert_equal __, array[5,0]
+    assert_equal [], array[4,100]
+    assert_equal [], array[5,0]
   end
 
   def test_arrays_and_ranges
