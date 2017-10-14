009b6b2163cab042871adca3cc2f01d2e5bec6cc
murmelmunz <munzinger3@gmail.com>
2017-10-14 14:04:19 +0200

    17/282

--- koans/about_arrays.rb
+++ koans/about_arrays.rb
@@ -38,7 +38,7 @@ class AboutArrays < Neo::Koan
     assert_equal [:peanut], array[0,1]
     assert_equal [:peanut, :butter], array[0,2]
     assert_equal [:and, :jelly], array[2,2]
-    assert_equal __, array[2,20]
+    assert_equal [:and, :jelly], array[2,20]
     assert_equal __, array[4,0]
     assert_equal __, array[4,100]
     assert_equal __, array[5,0]
