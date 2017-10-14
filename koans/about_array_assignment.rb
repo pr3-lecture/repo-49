c9a5addfd1fe19b031cd3cfc11c2408614a02713
murmelmunz <munzinger3@gmail.com>
2017-10-14 15:16:52 +0200

    17/282

--- koans/about_array_assignment.rb
+++ koans/about_array_assignment.rb
@@ -20,7 +20,7 @@ class AboutArrayAssignment < Neo::Koan
 
   def test_parallel_assignments_with_splat_operator
     first_name, *last_name = ["John", "Smith", "III"]
-    assert_equal "Jphn", first_name
+    assert_equal "John", first_name
     assert_equal "Smith", last_name
   end
 
